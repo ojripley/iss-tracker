ed945df392183f674cdf39ee8109eec8ad0b81aa
Owen Ripley <ojripley@gmail.com>
2019-09-26 13:52:28 -0400

    implmented the determineCoordinates function

--- index.js
+++ index.js
@@ -1,11 +1,18 @@
 // requires and runs the main code
-const { fetchIP } = require('./issSpotter');
+const { fetchIP, determineCoordinates } = require('./issSpotter');
 
-fetchIP((error, ip) => {
-  if (!error) {
-    console.log(ip);
-    return ip;
-  } else {
-    console.log('Something went wrong' + error);
-  }
-});
+// fetchIP((error, ip) => {
+//   if (!error) {
+//     return ip;
+//   } else {
+//     throw `\nSomething went wrong getting your IP address!\n\n ${error}`;
+//   }
+// });
+
+// determineCoordinates('66.207.199.230', (error, coordinates) => {
+//   if (!error) {
+//     console.log(coordinates);
+//   } else {
+//     throw `\nSomething went wrong determining your location!\n\n ${error}`;
+//   }
+// });
--- issSpotter.js
+++ issSpotter.js
@@ -5,7 +5,7 @@ const request = require('request');
 // returns the IP address of the network the user is on
 // the callback needs to be passed the ip address (string), the error code and the response status code
 const fetchIP = function(callback) {
-  request('https://api.ipify.org', (error, response, ip) => {
+  request('https://api.ipify.org/', (error, response, ip) => {
     
     // if there is an error, callback with error message and exit function
     // errors are likely to be invalid domain, user is offline
@@ -15,7 +15,7 @@ const fetchIP = function(callback) {
     }
 
     // made my own error message here in case of non 200 status code from server
-    if (response.statusCode !== 200 ) {
+    if (response.statusCode !== 200) {
       callback(`Server Error!\nStatus Code: ${response.statusCode}`, null);
     } else {
       // if all goes well, callback with the data and no error
@@ -24,4 +24,28 @@ const fetchIP = function(callback) {
   });
 };
 
-module.exports = { fetchIP };
+const determineCoordinates = function(ip, callback) {
+  request('https://ipvigilante.com/' + ip, (error, response, data) => {
+    
+    if (error) {
+      callback(error, null);
+      return;
+    }
+
+    if (response.statusCode !== 200) {
+      callback(`Server Error!\nStatus Code: ${response.statusCode}`, null);
+    } else {
+      // parse the JSON text into an object
+      const coordInfo = JSON.parse(data);
+      // extract only the latitude and longitude key/value pairs and make a new object out of them
+      const coordinates = {
+        latitude: coordInfo.data.latitude,
+        longitude: coordInfo.data.longitude
+      };
+      // 'coordinates' is now an object with two keys: latitude and longitude
+      callback(null, coordinates);
+    }
+  });
+};
+
+module.exports = { fetchIP, determineCoordinates };
